8f3213b7291f92132134d483349185872b342203
Julius Shrestha <41096619+juliusshrestha@users.noreply.github.com>
2024-08-13 11:18:18 +0545

    Update Attention.py

--- models/Attention.py
+++ models/Attention.py
@@ -45,43 +45,43 @@ class Attention(keras.layers.Layer):
         H,
         W,
     ):
-        get_shape = tf.keras.ops.shape(x)
+        get_shape = tf.shape(x)
         B = get_shape[0]
         C = get_shape[2]
 
         q = self.q(x)
-        q = tf.keras.ops.reshape(
-            q, (tf.keras.ops.shape(q)[0], -1, self.num_heads, self.head_dim)
+        q = tf.reshape(
+            q, (tf.shape(q)[0], -1, self.num_heads, self.head_dim)
         )
-        q = tf.keras.ops.transpose(q, axes=[0, 2, 1, 3])
+        q = tf.transpose(q, axes=[0, 2, 1, 3])
 
         if self.sr_ratio > 1:
-            x = tf.keras.ops.reshape(x, (B, H, W, C))
+            x = tf.reshape(x, (B, H, W, C))
             x = self.sr(x)
-            x = tf.keras.ops.reshape(x, (B, -1, C))
+            x = tf.reshape(x, (B, -1, C))
             x = self.norm(x)
 
         k = self.k(x)
-        k = tf.keras.ops.reshape(
-            k, (tf.keras.ops.shape(k)[0], -1, self.num_heads, self.head_dim)
+        k = tf.reshape(
+            k, (tf.shape(k)[0], -1, self.num_heads, self.head_dim)
         )
-        k = tf.keras.ops.transpose(k, axes=[0, 2, 1, 3])
+        k = tf.transpose(k, axes=[0, 2, 1, 3])
 
         v = self.v(x)
-        v = tf.keras.ops.reshape(
-            v, (tf.keras.ops.shape(v)[0], -1, self.num_heads, self.head_dim)
+        v = tf.reshape(
+            v, (tf.shape(v)[0], -1, self.num_heads, self.head_dim)
         )
-        v = tf.keras.ops.transpose(v, axes=[0, 2, 1, 3])
+        v = tf.transpose(v, axes=[0, 2, 1, 3])
 
-        attn = tf.keras.ops.matmul(q, k)
-        scale = tf.keras.ops.cast(self.sqrt_of_units, dtype=attn.dtype)
-        attn = tf.keras.ops.divide(attn, scale)
+        attn = tf.matmul(q, k)
+        scale = tf.cast(self.sqrt_of_units, dtype=attn.dtype)
+        attn = tf.divide(attn, scale)
 
-        attn = tf.keras.ops.softmax(attn, axis=-1)
+        attn = tf.softmax(attn, axis=-1)
         attn = self.attn_drop(attn)
-        x = tf.keras.ops.matmul(attn, v)
-        x = tf.keras.ops.transpose(x, axes=[0, 2, 1, 3])
-        x = tf.keras.ops.reshape(x, (B, -1, self.units))
+        x = tf.matmul(attn, v)
+        x = tf.transpose(x, axes=[0, 2, 1, 3])
+        x = tf.reshape(x, (B, -1, self.units))
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
